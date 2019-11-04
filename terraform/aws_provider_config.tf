6c2a594f581aaa5e70ec22947d58a91e984c9beb
Thomas McLaughlan <tmclaughlan@scottlogic.com>
2019-11-04 13:37:05 +0000

    Use an s3 bucket to coordinate terraform state

--- terraform/aws_provider_config.tf
+++ terraform/aws_provider_config.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "finos-cloud-services-terraform-state"
+    key = "terraform/state"
+    region = "eu-west-2"
+  }
+}
