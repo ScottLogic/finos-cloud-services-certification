40df6933569d786773f5c0716dedca414176e4d7
Thomas McLaughlan <tmclaughlan@scottlogic.com>
2019-11-04 14:57:48 +0000

    Pull out most of the common stuff to a module, configured with a handful of variables

---
+++ terraform/custom_rule_module/README.md
@@ -0,0 +1,7 @@
+Custom AWS Config Rule
+======================
+
+This module creates a custom AWS Config rule, which uses a lambda to execute a specified handler
+which is implemented in the Java application.
+
+The module will create a Lambda, IAM Policy, and an AWS Config Rule.
--- terraform/dynamodb_secure_transport_check.tf
+++ terraform/dynamodb_secure_transport_check.tf
@@ -1,73 +1,12 @@
-resource "aws_iam_role" "lambda_iam_test" {
-  name = "lambda_iam_role_for_rules_test"
+module "config_rule" {
+  source = "./custom_rule_module"
 
-  path = "/service-role/"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      }
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "role_iam_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
-  role = "${aws_iam_role.lambda_iam_test.name}"
-}
-
-resource "aws_iam_role_policy_attachment" "role_lambda_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role = "${aws_iam_role.lambda_iam_test.name}"
-}
-
-resource "aws_iam_role_policy_attachment" "role_config_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRulesExecutionRole"
-  role = "${aws_iam_role.lambda_iam_test.name}"
-}
-
-resource "aws_lambda_function" "lambda_test_1" {
-  s3_bucket = "${aws_s3_bucket.s3.id}"
-  s3_key = "${aws_s3_bucket_object.rules_jar.key}"
-  function_name = "lambda_rules_test_1"
+  name = "dynamodb-secure-transport-check"
+  additional_policies = ["arn:aws:iam::aws:policy/IAMReadOnlyAccess"]
+  s3 = {
+    bucket = aws_s3_bucket.s3.id
+    key    = aws_s3_bucket_object.rules_jar.key
+  }
   handler = "com.scottlogic.compliance.dynamodb.SecureTransportCheck"
-
-  runtime = "java8"
-
-  role = "${aws_iam_role.lambda_iam_test.arn}"
-
-  memory_size = 512
-  timeout = 15
-}
-resource "aws_lambda_permission" "lambda_iam_test_permission_1" {
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda_test_1.arn}"
-  principal     = "config.amazonaws.com"
-  statement_id  = "AllowExecutionFromConfig"
-}
-
-resource "aws_config_config_rule" "config_rule_1" {
-  name = "config_rule_test_1"
-  source {
-    owner = "CUSTOM_LAMBDA"
-    source_identifier = "${aws_lambda_function.lambda_test_1.arn}"
-    source_detail {
-      message_type = "ScheduledNotification"
-      maximum_execution_frequency = "TwentyFour_Hours"
-    }
-    source_detail {
-      message_type = "ConfigurationItemChangeNotification"
-    }
-  }
-  scope {
-    compliance_resource_types = ["AWS::IAM::User"]
-  }
+  resource_types = ["AWS::IAM::User"]
 }
